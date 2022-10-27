4372eb6a6b4c507bad20d0151f15f07b4accd0b1
贰狐RH <58497504+rhwong@users.noreply.github.com>
2022-10-27 18:00:43 +0800

    update

--- README.md
+++ README.md
@@ -4,10 +4,11 @@
 
 用于快速部署基于 [AmiyaBot](https://github.com/AmiyaBot/Amiya-Bot) 框架的 QQ 聊天机器人的Docker<br>
 
+注意：本项目仅用于快速部署AmiyaBot本体，后续对接何种前端(如[Go-cqhtp](https://github.com/Mrs4s/go-cqhttp/))取决您自己的选择，这些部分并不包含在Docker镜像内，请参考官方教程。
 
 </div>
 <!-- projectInfo end -->
 
 ## 简单开始
 
-暂未发布
+### 
--- bat/start.sh
+++ bat/start.sh
@@ -1,39 +1,56 @@
 #!/bin/bash
 
-#=================================================
-#	System Required: Docker Ubuntu
-#	Description: Amiya-Bot-Docker
-#	Version: 1.0.0
-#	Author: RHWong
-#=================================================
+echo =================================================
+echo	System Required: Docker Ubuntu
+echo	Description: Amiya-Bot-Docker
+echo	Version: 1.0.0
+echo	Author: RHWong
+echo =================================================
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
-Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
+Tip="${Green_font_prefix}[提示]${Font_color_suffix}"
 
+function waiting()
+{
+i=0
+while [ $i -le 100 ]
+do
+for j in '\\' '|' '/' '-'
+do
+printf "\t%c%c%c%c%c ${Info} 少女祈祷中... %c%c%c%c%c\r" \
+"$j" "$j" "$j" "$j" "$j" "$j" "$j" "$j" "$j" "$j"
+sleep 0.1
+done
+let i=i+4
+done
+}
 
-# 检查网络连通性
-    ping -c 1 -W ${Timeout} github.com > /dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        # 无法连接Github，使用镜像下载
-        echo -e "${Info} 无法连接Github，使用镜像下载" 
-        wget -N --no-check-certificate "https://ghproxy.com/https://raw.githubusercontent.com/AmiyaBot/Amiya-Bot/V6-master/requirements.txt"
-    else
-        # 连接Github，使用Github下载
-        echo -e "${Info} 连接Github，使用Github下载" 
+StartAmiya()
+{
+# 通过http状态码判断网络连通性
+    if [ $(curl -o /dev/null --connect-timeout 3 -s -w %{http_code} https://github.com) -eq 200 ]; then
+        echo -e "${Info} 网络连通性良好，使用Github下载"
         wget -N --no-check-certificate "https://raw.githubusercontent.com/AmiyaBot/Amiya-Bot/V6-master/requirements.txt"
+        pip install --upgrade pip -r requirements.txt & waiting
+    else
+        echo -e "${Info} 网络连通性不佳，使用镜像下载"
+        wget -N --no-check-certificate "https://ghproxy.com/https://raw.githubusercontent.com/AmiyaBot/Amiya-Bot/V6-master/requirements.txt"
+        pip install --upgrade pip -r requirements.txt -i https://mirrors.cloud.tencent.com/pypi/simple & waiting
     fi
 # 升级依赖
-pip install --upgrade pip -r requirements.txt
+
 
 # 启动Amiya
 cd /workspace/AmiyaBot && nohup python3 amiya.py >/dev/null 2>&1 &
 # 检测是否启动成功
-sleep 30
+waiting
 ps -ef | grep amiya.py | grep -v grep
 if [ $? -eq 0 ]; then
     echo -e "${Info} Amiya-Bot 启动成功" 
 else
     echo -e "${Error} Amiya-Bot 启动失败" && exit 1
 fi
+}
+StartAmiya
